8dcde49ee45cbda8a121cffe3be6c112cdb9ec3f
akinluyigithub <134658948+akinluyigithub@users.noreply.github.com>
2023-08-23 19:58:36 -0500

    Update EmployeeService.java

--- src/main/java/com/mt/services/EmployeeService.java
+++ src/main/java/com/mt/services/EmployeeService.java
@@ -3,6 +3,7 @@ package com.mt.services;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
@@ -16,15 +17,16 @@ public class EmployeeService {
 
     @RequestMapping(value = "/getEmployeeDetails", method = RequestMethod.GET)
     @ResponseBody
-    public String getEmployeeDetails(HttpServletRequest request, HttpServletResponse response, HttpSession httpSession)
-            throws JSONException {
+    public String getEmployeeDetails() throws JSONException {
         JSONObject employeeDetails = new JSONObject();
         employeeDetails.put("Name", "Jendarey Consulting Inc");
         employeeDetails.put("Calling Name", "JendareyTech");
         employeeDetails.put("DOB", "30-Apr-2022");
-        employeeDetails.put("Hobbies", "Watching Football, Teaching, Helping People...");
-        employeeDetails.put("Places he likes", "Africa, Church, His family");
+        employeeDetails.put("Hobbies", "Watching Football,Teaching, Helping People..");
+        employeeDetails.put("Places he like", "Africa, Church, His family");
+
         return employeeDetails.toString();
     }
 }
+
 
